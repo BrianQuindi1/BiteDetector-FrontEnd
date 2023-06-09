d636ca0f2b84d32c03328eeb63da4989cedef05f
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-09 09:03:19 -0300

    Camera Starting

--- BiteDetector/App.js
+++ BiteDetector/App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        flashMode={flash}
+        ref={camera}  
+      >
+        <Text>hello</Text>
+      </Camera>
+      <View>
+
+      </View>
     </View>
   );
 }
@@ -17,4 +43,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+    borderRadius: 20,
+  }
 });
